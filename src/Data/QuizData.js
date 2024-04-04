1803dff84b81d3ac040607000487fb14784a3b63
safique miya <safiquemiya1001@gmail.com>
2024-04-04 09:10:11 +0530

    massage

--- src/Data/QuizData.js
+++ src/Data/QuizData.js
@@ -24,4 +24,24 @@ export const QuizData = [
         options: ["Salman", "Sharukh", "Amir", "Amitab"],
         answer:2
     },
+    {
+        question: "Where is the tomb of Sher Shah Suri located ?",
+        options: ["Rohtasgarh ", "Sasaram ", "Chausa ", "Delhi"],
+        answer:2
+    },
+    {
+        question: "Who among the following is NOT associated with the Mughal empire ?",
+        options: ["Kamran Mirza ", "Sher Shah Suri ", " Shah Alam II  ", "Dara Sikoh "],
+        answer:3
+    },
+    {
+        question: ". 	When was the ' Battle of Kanauj' fought ?",
+        options: ["1540 ", "1524 ", "1536 ", "1556"],
+        answer:1
+    },
+    {
+        question: "Humayun’s heir, _______ , was born in exile and was only 13 years old when his father died ?",
+        options: ["Akbar ", " 	  Shah Jahan ", "Jahangir  ", " Babur "],
+        answer:1 
+    },
 ];
